e23f515829cbbcd6d25663be6c0d80b069d19c87
kocsis.mate <kocsismate@woohoolabs.com>
2017-02-15 12:58:38 +0100

    Added tests for the default serializer and deserializer

---
+++ tests/JsonApi/Serializer/JsonDeserializerTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace WoohooLabs\Yang\Tests\JsonApi\Serializer;
+
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use WoohooLabs\Yang\JsonApi\Serializer\JsonDeserializer;
+
+class JsonDeserializerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function deserialize()
+    {
+        $body = [
+            "data" => [
+                "type" => "a",
+                "id" => "1",
+            ]
+        ];
+
+        $response = $this->createResponse($body);
+        $deserializer = new JsonDeserializer();
+
+        $this->assertEquals($body, $deserializer->deserialize($response));
+    }
+
+    private function createResponse(array $body): Response
+    {
+        return new Response(200, [], json_encode($body));
+    }
+}
---
+++ tests/JsonApi/Serializer/JsonSerializerTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace WoohooLabs\Yang\Tests\JsonApi\Serializer;
+
+use GuzzleHttp\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+use WoohooLabs\Yang\JsonApi\Serializer\JsonSerializer;
+
+class JsonSerializerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function serialize()
+    {
+        $body = [
+            "data" => [
+                "type" => "a",
+                "id" => "1",
+            ]
+        ];
+
+        $response = $this->createRequest($body);
+        $serializer = new JsonSerializer();
+
+        $this->assertEquals(json_encode($body), $serializer->serialize($response, $body)->getBody()->__toString());
+    }
+
+    private function createRequest(array $body): Request
+    {
+        return new Request("", "", [], json_encode($body));
+    }
+}
