60ee62a87fc28eb2339bfb4a77b0fee06872d724
Fei Sun <feisun@hipagesgroup.com.au>
2021-10-11 21:52:51 +1100

    Implement PSR interface

--- src/JsonApi/Client/JsonApiClient.php
+++ src/JsonApi/Client/JsonApiClient.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace WoohooLabs\Yang\JsonApi\Client;
 
 use Http\Client\HttpClient;
+use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
 use WoohooLabs\Yang\JsonApi\Serializer\DeserializerInterface;
 use WoohooLabs\Yang\JsonApi\Serializer\JsonDeserializer;
 
-class JsonApiClient
+class JsonApiClient implements ClientInterface
 {
     /**
      * @var HttpClient
