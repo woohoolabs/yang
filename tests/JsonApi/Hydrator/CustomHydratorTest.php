6cc0b2ca1039f03fba9bbc4e1e620f13f8ea8e8e
Máté Kocsis <kocsismate@woohoolabs.com>
2023-08-19 23:15:24 +0200

    Use object shape instead of assertion

--- tests/JsonApi/Hydrator/CustomHydratorTest.php
+++ tests/JsonApi/Hydrator/CustomHydratorTest.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace WoohooLabs\Yang\Tests\JsonApi\Hydrator;
 
 use PHPUnit\Framework\TestCase;
-use stdClass;
 use WoohooLabs\Yang\JsonApi\Hydrator\ClassDocumentHydrator;
 use WoohooLabs\Yang\JsonApi\Hydrator\DocumentHydratorInterface;
 use WoohooLabs\Yang\JsonApi\Schema\Document;
 use WoohooLabs\Yang\JsonApi\Schema\Resource\ResourceObject;
-
-use function assert;
 
 class CustomHydratorTest extends TestCase
 {
@@ -29,9 +26,9 @@ class CustomHydratorTest extends TestCase
 
         $document = Document::fromArray($document);
         $hydrator = $this->getCustomHydrator();
+        /** @var object{type: string, id: string} $object */
         $object = $hydrator->hydrateSingleResource($document);
 
-        assert($object instanceof stdClass);
         $this->assertEquals("a", $object->type);
         $this->assertEquals("1", $object->id);
     }
